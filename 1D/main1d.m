535b484dcb8c2c93749255a34392b58b6ca06012
TRUONG Trong Phuc <nintendo_corp@msn.com>
2016-02-17 16:44:33 +0100

    1D swaggy swag

--- 1D/main1d.m
+++ 1D/main1d.m
@@ -2,19 +2,21 @@ clc
 clear
 
 % Constant (void)
-u = 4*pi*1e-12;
+u = 1.25663706*1e-6;
 eps = 8.8541878176e-12;
+c = 299792458;
+f = 2.4*1e9;
+lambda = c/f;
 
 % Option
-deltax = 0.0000000001;
-deltat = 0.0000000001;
-size = 100;
+deltax = lambda/10;
+deltat = deltax/c/10;
+size = 2000;
 
 % Initial condition
 
 hy = zeros(size,size);
 ez = zeros(size,size);
-ez(2,2) = 1;
 
 % hy(3,1) = 0;
 % hy(1,3) = 42;
@@ -22,8 +24,14 @@ ez(2,2) = 1;
 % ez(4,2) = 2;
 %%
 for t=2:2:size
+    t
+    if t < 500
+        ez(2,t) = cos(2*pi*f*deltat*(t-2));
+    end
     for x=2:2:size-2
         hy(x+1,t+1)= hy(x+1,t-1) + deltat*(ez(x+2,t)-ez(x,t))/(u*deltax);
+    end
+    for x=4:2:size-2
         ez(x,t+2)= ez(x,t) + deltat*(hy(x+1,t+1)-hy(x-1,t+1))/(eps*deltax);
     end
 end
