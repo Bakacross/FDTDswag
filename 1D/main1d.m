824df184b250f7b5df51f385996b5f661ba3309a
TRUONG Trong Phuc <nintendo_corp@msn.com>
2016-02-17 15:44:32 +0100

    x then t

--- 1D/main1d.m
+++ 1D/main1d.m
@@ -6,14 +6,14 @@ u = 4*pi*1e-12;
 eps = 8.8541878176e-12;
 
 % Option
-deltax = 0.001;
-deltat = 0.001;
+deltax = 0.0000000001;
+deltat = 0.0000000001;
 size = 100;
 
 % Initial condition
 
-hy = zeros(100,100);
-ez = zeros(100,100);
+hy = zeros(size,size);
+ez = zeros(size,size);
 ez(2,2) = 1;
 
 % hy(3,1) = 0;
@@ -21,9 +21,9 @@ ez(2,2) = 1;
 % ez(2,2) = 1;
 % ez(4,2) = 2;
 %%
-for t=2:2:4
-    x=t
-    hy(x+1,t+1)= hy(x+1,t-1) + deltat*(ez(x+2,t)-ez(x,t))/(u*deltax);
-    ez(x,t+2)= ez(x,t) + deltat*(hy(x+1,t+1)-hy(x-1,t+1))/(eps*deltax);
-    
+for t=2:2:size
+    for x=2:2:size-2
+        hy(x+1,t+1)= hy(x+1,t-1) + deltat*(ez(x+2,t)-ez(x,t))/(u*deltax);
+        ez(x,t+2)= ez(x,t) + deltat*(hy(x+1,t+1)-hy(x-1,t+1))/(eps*deltax);
+    end
 end
