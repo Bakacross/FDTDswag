f6629a7ebfe0d8632938be5f274cb171c043eede
MAHDJOUB  Fouad <fmahdjou@romeo34.ulb.ac.be>
2016-02-23 12:32:39 +0100

    2D

--- 2D/main2D.m
+++ 2D/main2D.m
@@ -1,35 +1,4 @@
-clc
-clear
 
-% Constant (void)
-u = 1.25663706*1e-6;
-eps = 8.8541878176e-12;
-c = 299792458;
-f = 2.4*1e9;
-lambda = c/f;
-
-% Option
-deltax = lambda/10;
-deltay = lambda/10;
-deltat = deltax/c/10;
-size = 2000;
-
-% Initial condition
-hx = zeros(size,size);
-hy = zeros(size,size);
-ez = zeros(size,size,size);
-
-% hy(3,1) = 0;
-% hy(1,3) = 42;
-% ez(2,2) = 1;
-% ez(4,2) = 2;
-%%
-for t=2:2:size
-    t
-    ez(2,2,t) = cos(2*pi*f*deltat*(t-2));
-    for x=2:2:size-2
-        hy(x+1,t+1)= hy(x+1,t-1) + deltat*(ez(x+2,t)-ez(x,t))/(u*deltax);
-    end
     
     clc
 clear
@@ -44,35 +13,58 @@ om = 1; %%% A definir becuz bs
 o = 1; %%% corriger bs fouad becuz it is bs hihihi
 % Option
 deltax = lambda/10;
+deltay = lambda/10;
 deltat = deltax/c/10;
-size = 2000;
+size = 100;
+x=size;
+y=x;
 c1 = (1-(om*deltat)/(2*u))/(1+(om*deltat)/(2*u));
 c2 = deltat/(u*deltay)/(1+(om*deltat)/(2*u));
 c3 = deltat/(u*deltax)/(1+(om*deltat)/(2*u));
 c4 = (1-(om*deltat)/(2*eps))/(1+(om*deltat)/(2*eps));
 c5 = 1/(1+(o*deltat/(2*eps)));
 % Initial condition
-hx = zeros(size,1);
-hy = zeros(size,1);
-ez = zeros(size,size,1);
+hx = zeros(x,y);
+hy = zeros(x,y);
+ez = zeros(x,y);
 
 % hy(3,1) = 0;
 % hy(1,3) = 42;
 % ez(2,2) = 1;
 % ez(4,2) = 2;
 %%
-for t=2:2:size
+
+for t=1:size
     t
-    ez(2,2,t) = cos(2*pi*f*deltat*(t-2));
-    for y=2:2:size-2
-        hx(x)= c1*hx(x) + c2*(ez(x)-ez(x,t))/(u*deltax);
-        for x=2:2:size-2
-            ez(x,t+2)= ez(x,t) + deltat*(hy(x+1,t+1)-hy(x-1,t+1))/(eps*deltax);
+    ez(1,1) = cos(2*pi*f*deltat*(t-1));
+    for x=1:size-2
+            x;
+        for y=1:size-2
+            y;
+            hx(x,y)=c1*hx(x,y)-c2*(ez(x,y+1)-ez(x,y));
+            hy(x,y)=c1*hy(x,y)+c3*(ez(x+1,y)-ez(x,y));
+            ez(x+1,y+1)=c4*ez(x+1,y+1)+c5*((deltat/(eps*deltax))*(hy(x+2,y+1)-hy(x+1,y+1)))-c5*((deltat/(eps*deltay))*(hx(x+1,y+2)-hx(x+1,y+1)));
         end
     end
-    
+    surf(1:size,1:size,hx)
+    drawnow
+    hold on;
 end
-    for x=4:2:size-2
-        ez(x,t+2)= ez(x,t) + deltat*(hy(x+1,t+1)-hy(x-1,t+1))/(eps*deltax);
-    end
-end
+
+
+
+
+
+% 
+%     ez(2,2,t) = cos(2*pi*f*deltat*(t-2));
+%     for y=2:2:size-2
+%         hx(y)= c1*hx(y) + c2*(ez(x)-ez(x,t))/(u*deltax);
+%         for x=2:2:size-2
+%             ez(x,t+2)= ez(x,t) + deltat*(hy(x+1,t+1)-hy(x-1,t+1))/(eps*deltax);
+%         end
+%     end
+%     
+% end
+%     for x=4:2:size-2
+%         ez(x,t+2)= ez(x,t) + deltat*(hy(x+1,t+1)-hy(x-1,t+1))/(eps*deltax);
+%     end
